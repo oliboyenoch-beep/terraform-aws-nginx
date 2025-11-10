10b368ece2006ab6f90f03117fc5ca6be7c3f8f0
oliboyenoch-beep <oliboyenoch@gmail.com>
2025-11-10 10:36:13 +0100

    trigger terraform deploy

--- main.tf
+++ main.tf
@@ -79,10 +79,3 @@ resource "aws_instance" "web" {
   }
 }
 
-# =======================
-# Output
-# =======================
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
